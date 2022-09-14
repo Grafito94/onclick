263893f871ca3b246186f5efb349fe3555d64a57
Santiago Gutierrez Cardona <104087575+Grafito94@users.noreply.github.com>
2022-09-14 14:27:10 -0500

    OnclickJS

---
+++ script.js
@@ -0,0 +1,16 @@
+function changeText(){
+    var etiquetaPerfil = document.querySelector("#superior")
+    console.log();
+    etiquetaPerfil.innerText = "Logout";
+
+}
+
+function remove(){
+    var submitButton = document.querySelector("#addDef");
+    console.log();
+    submitButton.remove();
+}
+
+function alerta(){
+    alert("Ninja was liked")
+}
